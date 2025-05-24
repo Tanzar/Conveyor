b45e1c828cfcff3ac454a045f972dcfc38d69db3
Tanzar <spakowski19@gmail.com>
2025-05-24 13:51:10 +0200

    streamable conveyor class base, to expand lated

---
+++ src/Stream/StreamableConveyor.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace Tanzar\Conveyor\Stream;
+
+use Illuminate\Database\Eloquent\Model;
+use Tanzar\Conveyor\Models\ConveyorStream;
+use Tanzar\Conveyor\Stream\Params\StreamParams;
+use Tanzar\Conveyor\Stream\Params\StreamParamsConfig;
+
+class StreamableConveyor
+{
+    private StreamParams $params;
+    
+    public function __construct(
+        private ConveyorStream $stream
+    ) {
+
+
+        $this->params = new StreamParams($stream->params);
+    }
+
+    /**
+     * Setup for params
+     * Options set in resulting object will be used to generate streams with command
+     * @return StreamParamsConfig
+     */
+    public static function getParamsConfig(): StreamParamsConfig
+    {
+        return new StreamParamsConfig([]);
+    }
+
+    /**
+     * Update stream values
+     * 
+     * @param mixed $model - if model is null all models set in stream will be recalculated
+     * @return bool information if stream was updated
+     */
+    public function update(?Model $model): bool
+    {
+        if ($model) {
+            return $this->updateSingle($model);
+        }
+        return false;
+    }
+
+    private function updateSingle(Model $model): bool
+    {
+        return false;
+    }
+
+    private function updateAll(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Access to params
+     * @return StreamParams
+     */
+    final protected function params(): StreamParams
+    {
+        return $this->params;
+    }
+}
