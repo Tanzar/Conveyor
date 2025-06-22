dd1143c1d2ec5958ae6a8bd2029de59353dce65e
Tanzar <spakowski19@gmail.com>
2025-06-22 11:28:26 +0200

    comments

--- src/Configs/ConveyorConfigInterface.php
+++ src/Configs/ConveyorConfigInterface.php
@@ -5,6 +5,11 @@ namespace Tanzar\Conveyor\Configs;
 
 interface ConveyorConfigInterface
 {
+    /**
+     * Adds new model to config
+     * @param string $class - class name, must extend Tanzar\Conveyor\Models\ConveyorModel
+     * @return ModelConfigInterface
+     */
     public function model(string $class): ModelConfigInterface;
 	
 }
--- src/Configs/ModelConfigInterface.php
+++ src/Configs/ModelConfigInterface.php
@@ -1,13 +1,36 @@
 <?php
 
 namespace Tanzar\Conveyor\Configs;
+use Tanzar\Conveyor\Exceptions\InvalidBuildFunctionException;
+use Tanzar\Conveyor\Exceptions\InvalidHandlerException;
 
 interface ModelConfigInterface
 {
 
+    /**
+     * Add handler to manage model when updating conveyor
+     * $handler needs one parameter with model class typehint
+     * @param callable $handler function called to manage model when updating conveyor
+     * @throws InvalidHandlerException
+     * @return ModelConfigInterface
+     */
     public function handler(callable $handler): self;
 
+    /**
+     * Add query to retrieve models when recalculating or initializing conveyor
+     * $build uses 2 parameters:
+     *  1) Laravels query builder
+     *  2) Tanzar\Conveyor\Params\Params, which will be passed into method by convayor to use parameters for specified conveyor
+     * @param callable $build - function called to build query
+     * @throws InvalidBuildFunctionException
+     * @return ModelConfigInterface
+     */
     public function query(callable $build): self;
 
+    /**
+     * Add relation to be loaded
+     * @param string[]|string $relation
+     * @return ModelConfigInterface
+     */
     public function relation(string|array $relation): self;
 }
